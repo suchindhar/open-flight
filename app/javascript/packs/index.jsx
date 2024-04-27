bc5ea86fe8fe000877aa82b5fd234ccf0a5db44e
suchindhar <56792748+suchindhar@users.noreply.github.com>
2024-04-27 21:54:38 +0530

    Update index.jsx

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import App from '../src/components/App'
+// app/javascript/packs/pages.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import App from '../components/App'; // Adjust the import path relative to your project structure
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Router>
-      <Route path="/" component={App}/>
+      <Route path="/" component={App} />
     </Router>,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+    document.body.appendChild(document.createElement('div'))
+  );
+});
